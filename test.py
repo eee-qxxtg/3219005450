1acc2f4a10a5bc3e92c8f817a3d98356e109eb23
eee-qxxtg <89849966+eee-qxxtg@users.noreply.github.com>
2021-09-15 22:31:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
+import unittest
+from main import test
 
+
+class MyTestCase(unittest.TestCase):
+    def test_something1(self):
+        self.assertEqual(test(), 0.70)
+
+    def test_something2(self):
+        self.assertEqual(test(), 0.71)
+
+    def test_something3(self):
+        self.assertEqual(test(), 0.79)
+
+    def test_something4(self):
+        self.assertEqual(test(), 0.65)
+
+    def test_something5(self):
+        self.assertEqual(test(), 0.55)
+
+
+if __name__ == '__main__':
+    unittest.main()
